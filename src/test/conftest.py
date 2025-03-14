6a7ef4c44149337bfbd22030ee30a2267562ada2
Emanuel <emanuel.ableitner@gmx.at>
2025-03-14 14:16:52 +0100

    skip linting in CI tests

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -2,7 +2,7 @@
     Pytest configuration
 """
 
-# pylint: disable=unused-wildcard-import, wildcard-import, wrong-import-position, wrong-import-order, protected-access
+# pylint: disable=wrong-import-position, wrong-import-order, missing-function-docstring
 
 # Adding source path to sys path
 import pathlib
@@ -14,7 +14,21 @@ sys.path.append(f"{pathlib.Path(__file__).parent.parent}")
 sys.path.append(f"{pathlib.Path(__file__).parent}")
 # pylint: enable=wrong-import-position, wrong-import-order
 
+import pytest
+
 os.environ["DEBUG"] = "True"
 
-# pylint: disable-next=unused-import
-from .fixtures import setup, clean_after_function
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skip-linting", action="store_true", default=False, help="skip the pylint test"
+    )
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--skip-linting"):
+        skip_pylint = pytest.mark.skip(reason="skipping code linting due to --skip-linting arg")
+        for item in items:
+            if item.name == "test_pylint":
+                item.add_marker(skip_pylint)
+
+# pylint: disable-next=unused-wildcard-import, wildcard-import
+from .fixtures import *
