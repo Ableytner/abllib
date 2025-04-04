4c6c0b803d844f44b9e1e9432b01d19320d85e8e
Ableytner <emanuel.ableitner@gmx.at>
2025-04-04 03:44:12 +0200

    fix tests

--- src/abllib/error/_general.py
+++ src/abllib/error/_general.py
@@ -32,7 +32,10 @@ class DirNotFoundError(CustomException):
 class InternalCalculationError(CustomException):
     """Exception raised when an internal calculation resulted in an unacceptable value"""
 
-    default_message = "Internal calculation resulted in an unacceptable value"
+    default_messages = {
+        0: "Internal calculation resulted in an unacceptable value",
+        1: "Internal calculation resulted in an unacceptable value {0}"
+    }
 
 class InternalFunctionUsedError(CustomException):
     """Exception raised when an internal function was used by an external project"""
