c5e8f6434a25c64c359da09debdb93388495adb0
Ableytner <56540036+Ableytner@users.noreply.github.com>
2025-04-16 10:54:01 +0200

    Add abllib.wrapper.deprecated decorator (#11)
    * add abllib.wrapper.deprecated
    
    * update .gitignore
    
    * add proper default deprecation message

--- src/test/wrapper_test.py
+++ src/test/wrapper_test.py
@@ -2,11 +2,13 @@
 
 # pylint: disable=function-redefined, consider-using-with
 
+import re
+import os
 from datetime import datetime
 
 import pytest
 
-from abllib import error, wrapper
+from abllib import error, log, wrapper
 from abllib.pproc import WorkerThread
 
 def test_lock():
@@ -181,7 +183,7 @@ def test_locks_underscore_names():
     assert not wrapper.NamedLock("_test2").locked()
 
 def test_singleuse():
-    """Ensure that singleuse cannot be initialized"""
+    """Ensure that singleuse works as expected"""
 
     @wrapper.singleuse
     def func1():
@@ -193,4 +195,80 @@ def test_singleuse():
         func1()
 
     with pytest.raises(error.CalledMultipleTimesError):
+        func1()
+
+def test_deprecated():
+    """Ensure that wrapper.deprecated works as expected"""
+
+    assert hasattr(wrapper, "deprecated")
+    assert callable(wrapper.deprecated)
+
+def test_deprecated_warning():
+    """Ensure that wrapper.deprecated.warning works as expected"""
+
+    log.initialize(log.LogLevel.DEBUG)
+    log.add_file_handler("test.log")
+
+    @wrapper.deprecated
+    def func1():
+        pass
+    func1()
+    with open("test.log", "r", encoding="utf8") as f:
+        assert re.search(r"The functionality .* is deprecated", f.read())
+
+    # reset test.log file
+    log.initialize(log.LogLevel.DEBUG)
+    log.add_file_handler("test.log")
+
+    @wrapper.deprecated.warning
+    def func1():
+        pass
+    func1()
+    with open("test.log", "r", encoding="utf8") as f:
+        assert re.search(r"The functionality .* is deprecated", f.read())
+
+    # reset test.log file
+    log.initialize(log.LogLevel.DEBUG)
+    log.add_file_handler("test.log")
+
+    @wrapper.deprecated("A custom deprecation message")
+    def func1():
+        pass
+    func1()
+    with open("test.log", "r", encoding="utf8") as f:
+        assert "A custom deprecation message" in f.read()
+
+    # reset test.log file
+    log.initialize(log.LogLevel.DEBUG)
+    log.add_file_handler("test.log")
+
+    @wrapper.deprecated.warning("A custom deprecation message")
+    def func1():
+        pass
+    func1()
+    with open("test.log", "r", encoding="utf8") as f:
+        assert "A custom deprecation message" in f.read()
+
+    log.initialize()
+    os.remove("test.log")
+
+def test_deprecated_error():
+    """Ensure that wrapper.deprecated.error works as expected"""
+
+    @wrapper.deprecated.error
+    def func1():
+        pass
+    with pytest.raises(error.DeprecatedError):
+        func1()
+
+    @wrapper.deprecated("A custom deprecation message", True)
+    def func1():
+        pass
+    with pytest.raises(error.DeprecatedError):
+        func1()
+
+    @wrapper.deprecated.error("A custom deprecation message")
+    def func1():
+        pass
+    with pytest.raises(error.DeprecatedError):
         func1()
