f7c4f35fe85f23c75c939214570fe0adb23440e4
Ableytner <emanuel.ableitner@gmx.at>
2025-06-12 11:21:29 +0200

    remove redundant locking from StorageView

--- src/abllib/storage/_storage_view.py
+++ src/abllib/storage/_storage_view.py
@@ -1,38 +1,13 @@
 """Module containing the _StorageView class"""
 
 from __future__ import annotations
-import functools
 from typing import Any
 
 from .._storage._base_storage import _BaseStorage
 from ._cache_storage import _CacheStorage
-from .. import error, wrapper
+from .. import error
 
 # pylint: disable=protected-access
-
-def _locking(func):
-    """Make a function require all storage locks to be held"""
-
-    @functools.wraps(func)
-    def inner(*args, **kwargs):
-        """The wrapped function that is called on function execution"""
-
-        self: _StorageView = args[0]
-
-        for storage in self._storages:
-            if storage._LOCK_NAME != "_BaseStorage":
-                lock = wrapper.NamedSemaphore(storage._LOCK_NAME)
-                lock.acquire()
-
-        try:
-            return func(*args, **kwargs)
-        finally:
-            for storage in self._storages:
-                if storage._LOCK_NAME != "_BaseStorage":
-                    lock = wrapper.NamedSemaphore(storage._LOCK_NAME)
-                    lock.release()
-
-    return inner
 
 class _StorageView():
     """A read-only view on both the PersistentStorage and VolatileStorage"""
@@ -64,7 +39,6 @@ class _StorageView():
 
         self._storages.append(storage)
 
-    @_locking
     def contains_item(self, key: str, item: Any) -> bool:
         """
         Checks whether a key within the storage equals an item.
@@ -77,7 +51,6 @@ class _StorageView():
                 return True
         return False
 
-    @_locking
     def contains(self, key: str) -> bool:
         """
         Checks whether a key exists within the storage.
@@ -90,13 +63,11 @@ class _StorageView():
                 return True
         return False
 
-    @_locking
     def __getitem__(self, key: str) -> Any:
         for storage in self._storages:
             if key in storage:
                 return storage[key]
         raise error.KeyNotFoundError.with_values(key)
 
-    @_locking
     def __contains__(self, key: str) -> bool:
         return self.contains(key)
