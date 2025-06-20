fd6dabd8aff98a6a6429c1ec432d4757391b16ef
Ableytner <emanuel.ableitner@gmx.at>
2025-06-20 15:35:01 +0200

    bump version

--- src/abllib/__init__.py
+++ src/abllib/__init__.py
@@ -8,7 +8,7 @@ from . import alg, error, fs, fuzzy, onexit, wrapper
 from .log import get_logger
 from .storage import CacheStorage, VolatileStorage, PersistentStorage, StorageView
 
-__version__ = "1.3.5rc1"
+__version__ = "1.3.6rc1"
 
 __exports__ = [
     alg,
