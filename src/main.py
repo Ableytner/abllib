1c7e97f4142c3716f608271b37f343537fc0f9b7
Ableytner <emanuel.ableitner@gmx.at>
2025-07-31 09:55:49 +0200

    bump version for release 1.4.0

--- src/main.py
+++ src/main.py
@@ -2,20 +2,18 @@
 
 # TODO:
 # UP NEXT
-# supply package number manually to release workflow
-# publish to pypi
 # replace all TypeError occurances with WrongTypeError
 
 # IMPORTANT BUT ANNOYING
 # add tests for threaded storage access
-# document and understand levenshtein_distance
+# document levenshtein_distance
 # document fuzzy score calculation
+# optimize _lock_wrapper._log_callstack (takes half of the runtime when assigning storage item)
 
 # TAKES A LOT OF TIME / NEEDED IN THE FUTURE
-# add abllib.type module with verify function, which compares a value to a given type hint
-# add a enforce_types wrapper for ensuring function parameter types, which internally uses abllib.type
-# go throught all TODO comments and add type validation if necessary
-# add async module with function to run async func in sync context
+# construct target type-specific function at runtime in types.enforce 
+
+# pylint: skip-file
 
 if __name__ == "__main__":
     pass
