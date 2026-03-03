13ea3b7a949bbf77ce404dc3933d57a7caefa5a1
Ableytner <emanuel.ableitner@gmx.at>
2026-03-03 19:58:19 +0100

    bump version, remove debug code

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@
 # add tests for threaded storage access
 # document levenshtein_distance
 # document fuzzy score calculation
-# optimize _lock_wrapper._log_callstack (takes half of the runtime when assigning storage item)
 # better implement functools.update_wrapper for Deprecated class
 
 # TAKES A LOT OF TIME / NEEDED IN THE FUTURE
@@ -16,14 +15,4 @@
 # pylint: skip-file
 
 if __name__ == "__main__":
-    from time import perf_counter_ns
-
-    from abllib import VolatileStorage, log
-
-    VolatileStorage.initialize()
-
-    start = perf_counter_ns()
-    for c in range(100000):
-        VolatileStorage[str(c)] = "test"
-    end = perf_counter_ns() - start
-    print(end)
+    pass
